81a4ee758f909f668ef2246834470eeed834dd48
Pankaj <pankzmail@gmail.com>
2021-02-16 20:04:12 +0530

    Update login.js

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useDispatch } from 'react-redux'
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
