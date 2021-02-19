7d613288aef4c839f225b6babae0c77125f72ad3
Pankaj <pankzmail@gmail.com>
2021-02-19 23:00:59 +0530

    Update actions.js

--- src/components/Registration/actions.js
+++ src/components/Registration/actions.js
@@ -1,5 +1,16 @@
-export const postRegistration = (payload) => dispatch => {
-    console.log({
-        payload
+import interceptor from '../../utils/interceptor.js';
+
+export const postRegistration = (payload) => async (dispatch) => {
+    return interceptor({
+        url: 'http://3.20.116.189/accounts/registration',
+        method: 'POST',
+        body: payload
+    }).then(res => {
+        console.log(res)
+    }).catch(err => {
+        const sessionID = 'abcdef';
+        localStorage.removeItem('tokenId');
+        localStorage.setItem('tokenId', sessionID);
+        return sessionID;
     })
 }
