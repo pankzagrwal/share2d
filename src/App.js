d084595923aaf64451aab35fbbb39784bb23b318
Pankaj <pankzmail@gmail.com>
2021-02-20 17:54:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from "react-router-dom";
 import BottomNavigation from './components/bottomNavigation/bottomNavigation.js';
 import Dashboard from './components/Dashboard/dashboard.js';
@@ -12,28 +13,35 @@ import Profile from './components/Profile/profile.js'
 import ReferBuddy from './components/ReferBuddy/referBuddy.js';
 import Login from './components/Login/login.js';
 
-export default function BasicExample() {
+const RouteWrapper = ({component: Component, ...rest}) => {
+  const authToken = window.localStorage && localStorage.getItem('authToken')
+    return (
+
+        // Show the component only when the user is logged in
+        // Otherwise, redirect the user to /signin page
+        <Route {...rest} render={props => (
+            authToken ?
+                <Component {...props} />
+            : <Redirect to="/login" />
+        )} />
+    );
+};
+
+export default function Routes() {
   return (
     <Router>
       <>
         <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
+          <RouteWrapper component={Dashboard} path='/'  exact/>
+          <RouteWrapper component={AddOffer} path='/addOffer'  exact/>
+          <RouteWrapper component={Profile} path='/profile'  exact/>
+          <RouteWrapper component={ReferBuddy} path='/referBuddy'  exact/>
+          <RouteWrapper component={Dashboard} path='/'  exact/>
           <Route exact path="/login">
             <Login />
           </Route>
           <Route exact path="/register">
             <Registration />
-          </Route>
-          <Route exact path="/addOffer">
-            <AddOffer />
-          </Route>
-          <Route exact path="/profile">
-            <Profile />
-          </Route>
-          <Route exact path="/referBuddy">
-            <ReferBuddy />
           </Route>
         </Switch>
 
