a8820ab870e262b18dfcca29b724e375735c67c6
Pankaj <pankzmail@gmail.com>
2021-02-26 20:37:31 +0530

    Update actions.js

--- src/components/AddOffer/actions.js
+++ src/components/AddOffer/actions.js
@@ -1,10 +1,15 @@
-const industries = [{
-    name: 'Industry One'
-}, {
-    name: 'Industry Two'
-}];
+import interceptor from '../../utils/interceptor.js';
 
-export const getIndustries = (payload) => dispatch => {
+export const getIndustries = () => async (dispatch) => {
+    const result = await interceptor({
+        url: '/accounts/industry'
+    })
+    const industries =  result?.data?.data?.results.map(item => {
+        return {
+            name: item.name
+        }
+    })
+    console.log({industries})
     dispatch({
         type: 'SET_INDUSTRIES',
         payload: industries
