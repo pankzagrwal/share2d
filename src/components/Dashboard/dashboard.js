6b937f32bba07090bbd8ce230f5b4c54e9d40b70
Pankaj <pankzmail@gmail.com>
2021-02-26 20:46:50 +0530

    Update dashboard.js

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-    Button,
     Container,
     Grid,
     Typography,
@@ -17,50 +16,43 @@ import Referee from '../Referee/referee.js';
 import Referal from '../Referal/referal.js';
 
 const useStyles = makeStyles((theme) => ({
+  backgroundContainer: {
+      backgroundColor: '#f64e60',
+      height: '200px',
+      color: 'white',
+  },
+  name: {
+      padding: theme.spacing(1)
+  },
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(-7),
     marginBottom: theme.spacing(7),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
-  summaryCard: {
-      flexWrap: 'noWrap'
+  box: {
+    borderRadius: '15px',
+    boxShadow: 'none',
+  },
+  received: {
+      backgroundColor: '#fff4de',
+      color: '#ffa800'
+  },
+  refered: {
+      backgroundColor: '#e1f0ff',
+      color: '#3699ff'
+  },
+  comission: {
+      backgroundColor: '#ffe2e5',
+      color: '#f64e60'
+  },
+  refer: {
+      backgroundColor: '#c9f7f5',
+      color: '#1bc5bd'
   }
 }));
 
-const summaryList = [
-    {
-        title: 'Customer Received',
-        value: 12
-    },
-    {
-        title: 'Customer Refereed',
-        value: 10
-    },
-    {
-        title: 'Commision Received',
-        value: 23
-    }
-]
-
-const SummaryCard = ({
-    title,
-    value
-}) => {
-    return (
-        <Card raised>
-            <CardContent>
-            <Typography variant="subtitle1" gutterBottom>
-               {title}
-            </Typography>
-            <Typography variant="overline" display="block">
-                {value}
-            </Typography>
-            </CardContent>
-        </Card>
-    )
-}
 
 const Dashboard = () => {
     const classes = useStyles();
@@ -70,27 +62,70 @@ const Dashboard = () => {
         history.push("/ReferBuddy");
     }
     return (
+        <>
+        <Grid container className={classes.backgroundContainer}>
+            <Grid item xs={12}>
+                <Grid container justify='space-between'>
+                    <Grid item>
+                        <Typography variant="h6" className={classes.name}>
+                            Welcome Pankaj
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
          <Container maxWidth='xs' justify='center' className={classes.paper}>
                 <Grid container spacing={3} justify='space-between' alignItems="center">
                     <Grid item xs={12}>
-                       <Grid container justify='space-between'>
-                            <Grid item>
-                                Welcome XYZ
+                        <Grid container justify="space-around" spacing={2} className={classes.summaryCard}>
+                            <Grid item xs={5}>
+                                <Card className={`${classes.box} ${classes.received}`}>
+                                    <CardContent>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        Received
+                                    </Typography>
+                                    <Typography variant="overline" display="block">
+                                        12
+                                    </Typography>
+                                    </CardContent>
+                                </Card>
                             </Grid>
-                            <Grid item>
-                                <Button variant="outlined" color="primary" onClick={handleRefer}>
-                                    Refer
-                                </Button>
+                            <Grid item xs={5}>
+                                <Card className={`${classes.box} ${classes.refered}`}>
+                                    <CardContent>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                     Refered
+                                    </Typography>
+                                    <Typography variant="overline" display="block">
+                                        3
+                                    </Typography>
+                                    </CardContent>
+                                </Card>
                             </Grid>
-                       </Grid>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Grid container justify="space-between" spacing={2} className={classes.summaryCard}>
-                            {summaryList.map(({title, value}) => (
-                                <Grid key={value} item>
-                                    <SummaryCard title={title} value={value} />
-                                </Grid>
-                            ))}
+                            <Grid item xs={5}>
+                                <Card className={`${classes.box} ${classes.comission}`}>
+                                    <CardContent>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        Comission
+                                    </Typography>
+                                    <Typography variant="overline" display="block">
+                                        100.43
+                                    </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={5}>
+                                <Card className={`${classes.box} ${classes.refer}`}>
+                                    <CardContent>
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        Refer
+                                    </Typography>
+                                    <Typography variant="overline" display="block" onClick={handleRefer}>
+                                        Customer
+                                    </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
                         </Grid>
                     </Grid>
                     <Grid item xs={12}>
@@ -105,6 +140,7 @@ const Dashboard = () => {
                 </Grid>
 
          </Container>
+        </>
     )
 }
 
