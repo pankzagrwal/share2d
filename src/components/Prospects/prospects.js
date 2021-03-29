0aee8944ae4bd4cb21ab0951041e2e12c1391bc9
Pankaj <pankzmail@gmail.com>
2021-03-29 18:24:29 +0530

    Update prospects.js

--- src/components/Prospects/prospects.js
+++ src/components/Prospects/prospects.js
@@ -11,7 +11,9 @@ import {
     Paper,
     InputBase,
     Divider,
-    IconButton
+    IconButton,
+    Tabs,
+    Tab
 } from '@material-ui/core';
 import Send from '@material-ui/icons/Send';
 
@@ -68,7 +70,7 @@ const profilePicMap = {
   6: Six
 }
 
-const Prospect = () => {
+const ProspectComing = () => {
   const classes = useStyles();
   const profile = React.useMemo(() => Math.floor(Math.random() * 6) + 1, []);
   const [isExpandable, setIsExpandable] = React.useState(false);
@@ -94,64 +96,111 @@ const Prospect = () => {
             Description goes here about the requirement
           </span>
           <span>
-            Shop Name goes here
+            Refered by: ABC Industries
           </span>
         </Grid>
       </Grid>
       <Grid item xs={2} className={classes.dot} onClick={() => setIsExpandable(!isExpandable)}>
         <span >...</span>
       </Grid>
-      <Grid item xs={10}>
-        <Collapse in={isExpandable}>
-          <Grid container className={classes.collapse}>
-            <Grid item xs={12}>
-              <RadioGroup name="status" value={status} onChange={handleChange} color='primary' row className={classes.radioGroup}>
-                <FormControlLabel value="notSold" control={<Radio size='small'/>} label="Not Sold" />
-                <FormControlLabel value="sold" control={<Radio size='small'/>} label="Sold" />
-              </RadioGroup>
-            </Grid>
-            {
-              status === 'sold' &&
-              <Grid item xs={6}>
-                  <Paper component="form" className={classes.paper}>
-                    <InputBase
-                      className={classes.input}
-                      placeholder="Sold Amount"
-                    />
-                    <Divider className={classes.divider} orientation="vertical" />
-                    <IconButton color="primary">
-                      <Send fontSize="small"/>
-                    </IconButton>
-                  </Paper>
+        <Grid item xs={10}>
+          <Collapse in={isExpandable}>
+            <Grid container className={classes.collapse}>
+              <Grid item xs={12}>
+                <RadioGroup name="status" value={status} onChange={handleChange} color='primary' row className={classes.radioGroup}>
+                  <FormControlLabel value="notSold" control={<Radio size='small'/>} label="Not Sold" />
+                  <FormControlLabel value="sold" control={<Radio size='small'/>} label="Sold" />
+                </RadioGroup>
               </Grid>
-            }
-          </Grid>
-        </Collapse>
+              {
+                status === 'sold' &&
+                <Grid item xs={6}>
+                    <Paper component="form" className={classes.paper}>
+                      <InputBase
+                        className={classes.input}
+                        placeholder="Sold Amount"
+                      />
+                      <Divider className={classes.divider} orientation="vertical" />
+                      <IconButton color="primary">
+                        <Send fontSize="small"/>
+                      </IconButton>
+                    </Paper>
+                </Grid>
+              }
+            </Grid>
+          </Collapse>
+        </Grid>
+    </Grid>
+  )
+}
+
+const ProspectSent = () => {
+  const classes = useStyles();
+  const profile = React.useMemo(() => Math.floor(Math.random() * 6) + 1, []);
+  return (
+    <Grid container justify={'space-between'} alignItems='center' spacing={1} direction='row' className={classes.container}>
+      <Grid item xs={2}>
+        <Button href={'tel:9552530381'} className={classes.buttonBlock}>
+          <Avatar src={profilePicMap[profile]} className={classes.small}/>
+        </Button>
+      </Grid>
+      <Grid item container xs={10}  alignItems='center'>
+        <Grid item container direction='row' alignItems='center'>
+          <Typography variant='subtitle2'>
+            Ricky Ponting
+          </Typography>
+        </Grid>
+        <Grid item container  className={classes.textMuted} direction='column'>
+          <span>
+            Description goes here about the requirement
+          </span>
+          <span>
+            Referred To: XYZ Industries
+          </span>
+        </Grid>
       </Grid>
     </Grid>
   )
 }
 
 const Prospects = () => {
-
+    const [prospectsId, setProspectsId] = React.useState(0)
+    const  handleProspectsChange = (evt, value) => {
+      console.log(value)
+      setProspectsId(value)
+    }
     return (
       <Grid container>
         <Grid item xs={12}>
           <Typography variant='h6' gutterBottom>
-            Current Prospects
+            Active Prospects
           </Typography>
         </Grid>
         <Grid item xs={12}>
-          <Typography variant='body2' gtterBottom>
-            Reach out to prospects before they reach
-          </Typography>
+          <Tabs value={prospectsId} onChange={handleProspectsChange}>
+            <Tab label="Coming"  id = {0} />
+            <Tab label="Sent" id = {1} />
+          </Tabs>
         </Grid>
-        <Grid item xs={12}>
-          <Prospect />
-          <Prospect />
-          <Prospect />
-          <Prospect />
-        </Grid>
+        {
+          prospectsId === 0 &&
+          <Grid item xs={12}>
+            <ProspectComing />
+            <ProspectComing />
+            <ProspectComing />
+            <ProspectComing />
+          </Grid>
+        }
+        {
+          prospectsId === 1 &&
+          <Grid item xs={12}>
+            <ProspectSent />
+            <ProspectSent />
+            <ProspectSent />
+            <ProspectSent />
+            <ProspectSent />
+          </Grid>
+        }
         <Grid item xs={10}>
           <Button  color='primary'>View All</Button>
         </Grid>
