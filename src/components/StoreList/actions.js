3efd4476f9ef58385f1a7989ffd78a117da3a6c7
Pankaj <pankzmail@gmail.com>
2021-04-17 17:42:53 +0530

    Update actions.js

--- src/components/StoreList/actions.js
+++ src/components/StoreList/actions.js
@@ -4,8 +4,42 @@ export const getStores = (payload) => async (dispatch) => {
     // const  {
     //     type
     // } = payload;
-     await interceptor({
-        url: `/accounts/store?type=business`,
+    const stores = await interceptor({
+            url: `/accounts/store?type=business`,
+            method: 'GET',
+        })
+    const {
+        data: {
+            results,
+            count
+        } = {}
+    } = stores
+    const resultMap = results.reduce((acc, item) => {
+        return {
+            ...acc,
+            [item.id]: item
+        }
+    }, {})
+
+    const storeIds = Object.keys(resultMap).join(',')
+    const {
+        data: {
+            results: offers = []
+        } = {}
+    } = await interceptor({
+        url: `/offer/offer?business_ids=${storeIds}`,
         method: 'GET',
+    })
+    offers.forEach(offer => {
+        if (!resultMap[offer.business].offer) {
+             resultMap[offer.business].offer = offer
+        }
+    });
+    dispatch({
+        type: 'SET_STORES',
+        payload: {
+            results: Object.values(resultMap),
+            count
+        }
     })
 }
