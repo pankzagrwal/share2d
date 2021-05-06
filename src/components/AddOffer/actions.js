2995209d6b47f6a4c739617ea3d8523e75f84d0c
Pankaj <pankzmail@gmail.com>
2021-05-06 17:25:22 +0530

    Update actions.js

--- src/components/AddOffer/actions.js
+++ src/components/AddOffer/actions.js
@@ -3,10 +3,9 @@ import interceptor from '../../utils/interceptor.js';
 export const saveOffer = (payload) => async (dispatch) => {
     const url = payload.id ? `/offer/offer${`/${payload.id}`}` : '/offer/offer';
     const method = payload.id ? 'patch' : 'post'
-    const result = await interceptor({
+    return await interceptor({
         url,
         method,
         body: payload
     })
-    console.log(result)
 }
