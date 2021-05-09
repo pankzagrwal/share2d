1181795accd0f729f3589edf1cc67f80be58864b
Pankaj <pankzmail@gmail.com>
2021-05-09 16:47:08 +0530

    Update actions.js

--- src/components/LeadDialog/actions.js
+++ src/components/LeadDialog/actions.js
@@ -2,10 +2,27 @@ import interceptor from '../../utils/interceptor.js';
 
 
 export const dropLead = (payload) => async (dispatch) => {
+    dispatch({
+        type: 'SET_LOADER',
+        payload: true
+    })
      return await interceptor({
             url: `/lead/lead`,
             method: 'post',
             body: payload
+        }).then(() => {
+                dispatch({
+                    type: 'SET_LOADER',
+                    payload: false
+                })
+                dispatch({
+                    type: 'SET_ALERT',
+                    payload: {
+                        isOpen: true,
+                        severity: 'success',
+                        message: 'Lead Sent'
+                    }
+                })
         })
 
 }
