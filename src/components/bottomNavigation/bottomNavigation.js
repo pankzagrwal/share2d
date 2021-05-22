31b3239c2cc76b8a45d2e46c2f8a4aeacbf84664
Pankaj <pankzmail@gmail.com>
2021-05-22 15:32:05 +0530

    Update bottomNavigation.js

--- src/components/bottomNavigation/bottomNavigation.js
+++ src/components/bottomNavigation/bottomNavigation.js
@@ -5,7 +5,7 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import Home from '@material-ui/icons/Home';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Group from '@material-ui/icons/Group';
-// import MonetizationOn from '@material-ui/icons/MonetizationOn';
+import MonetizationOn from '@material-ui/icons/MonetizationOn';
 import {
   Link,
   useLocation
@@ -32,7 +32,6 @@ export default function Navigation () {
     const [value, setValue] = React.useState(0);
 
     React.useEffect(() => {
-        console.log(path.indexOf(pathname))
         setValue(path.indexOf(pathname))
     }, [pathname])
 
@@ -45,7 +44,7 @@ export default function Navigation () {
                 <BottomNavigationAction label="Home" value={0} icon={<Home />} component={Link} to={path[0]}/>
                 <BottomNavigationAction label="Profile" value={1} icon={<AccountCircle />} component={Link} to={path[1]}/>
                 <BottomNavigationAction label="Buddies" value={2} icon={<Group />}  component={Link} to={path[2]}/>
-                {/* <BottomNavigationAction label="Commission" value={3} icon={<MonetizationOn />}  component={Link} to={path[3]}/> */}
+                <BottomNavigationAction label="Commission" value={3} icon={<MonetizationOn />}  component={Link} to={path[3]}/>
             </BottomNavigation>
     )
 }
