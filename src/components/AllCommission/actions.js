9018bb9b3215e96cb19c48df89dc141cb578f0e0
Pankaj <pankzmail@gmail.com>
2021-07-28 18:26:52 +0530

    Update actions.js

--- src/components/AllCommission/actions.js
+++ src/components/AllCommission/actions.js
@@ -2,7 +2,19 @@ import interceptor from '../../utils/interceptor.js';
 
 export const getTransactions = (id) => async (dispatch) => {
     const {data} = await interceptor({
-        url: `/payments/transaction?ledger_ids=${id}`
+        url: `/payments/transaction?ledger_ids=${id}&types=1`
+    })
+
+
+    dispatch({
+        type: 'SET_TRANSACTION_MERCHANT',
+        payload: data?.results ?? []
+    })
+}
+
+export const getSettledTransaction = (id) => async (dispatch) => {
+    const {data} = await interceptor({
+        url: `/payments/transaction?ledger_ids=${id}&types=2`
     })
 
 
