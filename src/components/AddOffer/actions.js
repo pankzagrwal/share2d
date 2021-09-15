4c0d365d618e3220fdcf05b7494295e48bdd2ede
Pankaj <pankzmail@gmail.com>
2021-09-15 17:25:00 +0530

    Update actions.js

--- src/components/AddOffer/actions.js
+++ src/components/AddOffer/actions.js
@@ -3,6 +3,11 @@ import interceptor from '../../utils/interceptor.js';
 export const saveOffer = (payload) => async (dispatch) => {
     const url = payload.id ? `/offer/offer${`/${payload.id}`}` : '/offer/offer';
     const method = payload.id ? 'patch' : 'post'
+    try {
+    dispatch({
+        type: 'SET_LOADER',
+        payload: true
+    })
     const offer = await interceptor({
         url,
         method,
@@ -13,6 +18,33 @@ export const saveOffer = (payload) => async (dispatch) => {
         type: 'UPDATE_OFFER',
         payload: offer?.data
     })
+    dispatch({
+        type: 'SET_ALERT',
+        payload: {
+            isOpen: true,
+            severity: 'success',
+            message: 'Offer Updated'
+        }
+    })
+    dispatch({
+        type: 'SET_LOADER',
+        payload: false
+    })
 
     return offer;
+    }
+    catch {
+    dispatch({
+        type: 'SET_LOADER',
+        payload: false
+    })
+                dispatch({
+                type: 'SET_ALERT',
+                payload: {
+                    isOpen: true,
+                    severity: 'error',
+                    message: 'Offer Update  Failed'
+                }
+            })
+    }
 }
